7d79f67b655e7bf78ad87518224d7f81f37acce3
Mar3y <ahmed.mariee.2002@gmail.com>
2025-07-26 14:33:57 +0300

    paginated the articles

--- Modules/Articles/Repositories/ArticleRepository.php
+++ Modules/Articles/Repositories/ArticleRepository.php
@@ -28,7 +28,7 @@ class ArticleRepository implements CrudRepository
      */
     public function all()
     {
-        return Article::filter($this->filter)->orderBy('id', 'DESC')->get();
+        return Article::filter($this->filter)->orderBy('id', 'DESC')->paginate(20);
     }
 
     /**
